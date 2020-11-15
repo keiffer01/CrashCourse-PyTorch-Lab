1292b69d56c8a458c78c5444999ebf5d935715b4
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-11-15 05:19:39 -0500

    Modify hyperparameters

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from net import MyNet
 ### TODO: Play around with these for different effects on training!
 
 # Number of times to repeat the training loop.
-num_epoch = 5
+num_epoch = 50
 
 # How many images to feed into the network at a time. Lower is more accurate,
 # but much slower to train. Should not exceed 50000! (the size of the image set)
